63333b952b1572ab81f446ef21e71222694c95e4
jeo <jeo.dogbert@gmail.com>
2025-08-07 19:46:21 +0000

    fix

--- app/__main__.py
+++ app/__main__.py
@@ -1,22 +1,22 @@
 from uptime_kuma_api import UptimeKumaApi
-from utils import create_args, add_monitor
+from utils import create_args, add_monitor, process_yml
 
 
 if __name__ == "__main__":
     args = create_args()
 
+    container_name_list = []
     if args.yaml_file != None:
-        pass
+        container_name_list = process_yml(args.yaml_file)
 
-    """
     with UptimeKumaApi(args.kuma_url) as api:
-        api.login(args.kuma_username, args.kuma_password)
-        add_monitor(
-            api,
-            args.monitor_type_str,
-            args.name,
-            hostname=args.hostname,
-            url=args.url,
-            docker_container_name=args.docker_container_name,
-        )
-    """
+        if len(container_name_list) == 0:
+            api.login(args.kuma_username, args.kuma_password)
+            add_monitor(
+                api,
+                args.monitor_type_str,
+                args.name,
+                hostname=args.hostname,
+                url=args.url,
+                docker_container_name=args.docker_container_name,
+            )
