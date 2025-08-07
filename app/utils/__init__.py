d7b833200d5880f8e744da491d73596f6fb67963
jeo <jeo.dogbert@gmail.com>
2025-08-07 19:31:02 +0000

    reorg

---
+++ app/utils/__init__.py
@@ -0,0 +1,4 @@
+from .arg_parser import create_args
+from .add_monitor import add_monitor
+
+__all__ = [create_args, add_monitor]
---
+++ app/utils/arg_parser.py
@@ -0,0 +1,42 @@
+import argparse
+
+
+def create_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-t",
+        "--monitor_type_str",
+        type=str,
+        help="Monitor type. Currently supported types are: ping, https, docker",
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        type=str,
+    )
+    parser.add_argument(
+        "-hn",
+        "--hostname",
+        default=None,
+        type=str,
+    )
+    parser.add_argument(
+        "-u",
+        "--url",
+        default=None,
+        type=str,
+    )
+    parser.add_argument(
+        "-d",
+        "--docker_container_name",
+        type=str,
+    )
+
+    parser.add_argument("--kuma_url")
+    parser.add_argument("--kuma_username")
+    parser.add_argument("--kuma_password")
+
+    args = parser.parse_args()
+
+    return args
